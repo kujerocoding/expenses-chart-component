f14634fd8f64f2b401362b9d1ba7a1f4423af4fd
herom <jerome.manset22@gmail.com>
2023-02-16 02:11:52 +0800

    add sample data file

---
+++ src/ExpensesData.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        id: 1,
+        day: "mon",
+        amount: 1
+    },
+    {
+        id: 2,
+        day: "tue",
+        amount: 2
+    },
+    {
+        id: 3,
+        day: "wed",
+        amount: 3
+    },
+    {
+        id: 4,
+        day: "thu",
+        amount: 4
+    },
+    {
+        id: 5,
+        day: "fri",
+        amount: 5
+    },
+    {
+        id: 6,
+        day: "sat",
+        amount: 6
+    },
+    {
+        id: 7,
+        day: "sun",
+        amount: 7
+    },
+]
--- src/components/ExpensesBarChart.jsx
+++ src/components/ExpensesBarChart.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Bar } from 'react-chartjs-2'
+import { Chart } from 'chart.js'
 
 const ExpensesBarChart = () => {
   return (
