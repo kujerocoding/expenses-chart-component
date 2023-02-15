176f64c46831e1f430720f9e8482f0ef931d6bb3
herom <jerome.manset22@gmail.com>
2023-02-16 07:57:34 +0800

    finalize attribution style

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
         </div>
       </div>
       <div className='attribution'>
-            Challenge by <a href="https://www.frontendmentor.io/home" target="_blank">Frontend Mentor.</a> Coded by kujerocoding
+            Challenge by <a href="https://www.frontendmentor.io/challenges" target="_blank">Frontend Mentor.</a> Coded by <a href='https://github.com/kujerocoding' target="_blank">kujerocoding</a>
         </div>
     </div>
   )
